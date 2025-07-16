20798fc490d78a5ecd8ddc1b845436169f559cfa
Akeem Mudashiru <akeemmudash@gmail.com>
2025-07-16 10:47:41 +0100

    Synced Code with tutor's repo

--- src/home/views.py
+++ src/home/views.py
@@ -29,3 +29,22 @@ def about_view(request, *arg, **kwargs):
     PageVisit.objects.create(path=path)
     html_template = 'home.html'
     return render(request, html_template, my_context)
+
+
+def my_old_home_page_view(request, *args, **kwargs):
+    my_title = "My Page"
+    my_context = {
+        "page_title": my_title
+    }
+    html_ = """
+    <!DOCTYPE html>
+<html>
+
+<body>
+    <h1>{page_title} anything?</h1>
+</body>
+</html>    
+""".format(**my_context)  # page_title=my_title
+    # html_file_path = this_dir / "home.html"
+    # html_ = html_file_path.read_text()
+    return HttpResponse(html_)
