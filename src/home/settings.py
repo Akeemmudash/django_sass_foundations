1c5b8342e32ac04c42e2f6d3e0ceda14f31824de
Akeem Mudashiru <akeemmudash@gmail.com>
2025-07-17 21:48:01 +0100

    Added command for downloading static files over network

--- src/home/settings.py
+++ src/home/settings.py
@@ -48,7 +48,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+
     # my-apps
+    "commando.apps.CommandoConfig",
     "visits.apps.VisitsConfig"
 ]
 
@@ -138,6 +140,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_BASE_DIR = BASE_DIR/"staticfiles"
+STATICFILES_BASE_DIR.mkdir(exist_ok=True, parents=True)
 STATICFILES_VENDOR_DIR = STATICFILES_BASE_DIR/"vendors"
 
 # source(s) for python manage.py collectstatic
